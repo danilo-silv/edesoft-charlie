adad950d4524e250d071de47bcfe6a2d11f07413
Danilo Silva <danilo.silva@laborit.com.br>
2022-09-01 21:55:34 -0300

    ✨ (feat): inserting atomic design
    Changelog: Added

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
+import App from './components/organisms/App'
 
 import './styles/base.css'
 import 'tailwindcss/tailwind.css'
